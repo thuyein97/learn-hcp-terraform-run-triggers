44549d9d2c4d94c9431d6cd8a5abf9d12536588c
ARS - Thu Yein Tin <111072736+thuyein97@users.noreply.github.com>
2025-10-30 09:07:27 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = data.tfe_outputs.source_workspace.nonsensitive_values.instance_security_group_ids
+  vpc_security_group_ids = [data.tfe_outputs.source_workspace.nonsensitive_values.instance_security_group_ids]
   subnet_id              = data.tfe_outputs.source_workspace.nonsensitive_values.instance_subnet
 
   tags = {
